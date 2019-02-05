998f424abfa6598e82fedf408ee0df2d256b4675
hiroshi <hiroshi@wovn.io>
2019-02-05 11:48:14 +0900

    Adjust go

--- src/Go.hs
+++ src/Go.hs
@@ -11,9 +11,19 @@ main = do
   case ret of
     String go_src -> do
       let path = "/tmp/tmp.go"
-      writeFile path go_src
+      writeFile path $ go_layout ++ go_src
       runCommand $ "go run " ++ path
     ret -> do
       dump_env env
       print $ show ret
       fail "Compile Error"
+
+ where
+  go_layout = unlines [
+      "package main"
+    , "import \"fmt\""
+    , "func main() {"
+    , "  ret := vl_main()"
+    , "  fmt.Printf(\"%v\\n\", ret)"
+    , "}"
+    ]
